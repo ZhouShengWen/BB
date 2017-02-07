e8d2f0e0835d62e1efed7529a690dd129e9c6aaa
ZhouShengWen <578631692@qq.com>
2017-02-07 17:38:36 +0800

    备注

--- src/com/zsw/bbservers/ZServers.java
+++ src/com/zsw/bbservers/ZServers.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.zsw.tools.cspring;
+
 public class ZServers extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		System.out.println("1");
+		cspring.ac();
 	}
 }
--- src/com/zsw/tools/cspring.java
+++ src/com/zsw/tools/cspring.java
@@ -4,6 +4,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class cspring {
+	
+	/**
+	 * 使用该静态方法换回一个ApplicationContext
+	 * */
 	public static ApplicationContext ac() {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-mybatis.xml");
 		return ac;
